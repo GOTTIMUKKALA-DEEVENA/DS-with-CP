d1bfb6331a1c8093a767545466c72ccd064e6711
Deevena G <deevena19029@gmail.com>
2026-03-09 10:51:29 +0530

    Update graph.java

--- Module1_Array/graph.java
+++ Module1_Array/graph.java
@@ -1,29 +1,34 @@
-import java.util.LinkedList;
+
+  import java.util.LinkedList;
 
 class Graph {
 
-    int V; // vertices
+    int V; // number of vertices
     LinkedList<Integer> adj[]; // adjacency list
 
-    Graph(int v){
-        this.V = v;
+    @SuppressWarnings("unchecked")
+    Graph(int v) {
+        V = v;
 
         adj = new LinkedList[V];
 
-        for(int i = 0; i < V; i++){
-            adj[i] = new LinkedList<>();
+        for (int i = 0; i < V; i++) {
+            adj[i] = new LinkedList<Integer>();
         }
     }
 
-    void addEdge(int v, int w){
+    // function to add edge
+    void addEdge(int v, int w) {
         adj[v].add(w);
     }
 
-    void printGraph(){
-        for(int i = 0; i < V; i++){
-            System.out.print(i + " -> ");
+    // function to print graph
+    void printGraph() {
+        for (int i = 0; i < V; i++) {
 
-            for(int j = 0; j < adj[i].size(); j++){
+            System.out.print(i + "->");
+
+            for (int j = 0; j < adj[i].size(); j++) {
                 System.out.print(adj[i].get(j) + " ");
             }
 
@@ -32,17 +37,18 @@ class Graph {
     }
 }
 
-class Main {
+public class Main {
+
     public static void main(String[] args) {
 
-        System.out.println("Try programiz.pro");
+       
 
         Graph g = new Graph(4);
 
-        g.addEdge(0,1);
-        g.addEdge(0,2);
-        g.addEdge(1,2);
-        g.addEdge(2,3);
+        g.addEdge(0, 1);
+        g.addEdge(0, 2);
+        g.addEdge(1, 2);
+        g.addEdge(2, 3);
 
         g.printGraph();
     }
